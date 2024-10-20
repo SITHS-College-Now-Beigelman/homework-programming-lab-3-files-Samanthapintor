c6e4579ac4920a464511f7f8955ad4abbc1b639f
Samanthapintor <samanthap329@nycstudents.net>
2024-10-20 13:39:39 -0400

    Update Samantha_Pintor_Homework_3.cpp

--- Samantha_Pintor_Homework_3.cpp
+++ Samantha_Pintor_Homework_3.cpp
@@ -1,6 +1,7 @@
 //Samantha Pintor
 //HW 3 
-//10-19-24
+//10-20-24
+
 
 #include <iostream>
 #include <fstream> 
@@ -13,48 +14,61 @@ int main ()
     ifstream inFile; //input file stream variable
 
     //creating variables 
-    int startingAmount; //variable for starting amount  
+    int totalMoney; //variable for starting amount  
     char transactionType; //variable for transaction type
-    float balance, numberOfTransactions, transactionAmount; 
-   
+    float numberOfTransactions, transactionAmount; 
+
     //open the files 
     inFile.open("inData.txt");
 
     //read the starting amount 
-    inFile >> startingAmount; 
-    balance = startingAmount; 
-    
+    inFile >> totalMoney; 
+
+    cout  << totalMoney << endl; //cout starting amount
+
+    cout << "TYPE......AMOUNT......BALANCE" << endl; //setting up the format
+
+    setfill ("."); //set fill to periods 
+
     //run the code for 7 transactions 
     for (numberOfTransactions = 0; numberOfTransactions < 7; numberOfTransactions ++)
+    {
         inFile >> transactionType >> transactionAmount; //variables for inputs 
-    
-    cout << "TYPE......AMOUNT......BALANCE" << endl; //setting up the format
-    
-    setfill ("."); //set fill to periods 
-    
+
     //when transaction is withdrawal
     if (transactionType == 'W')
     {
-        balance -= transactionAmount; //updatting the balance
-        cout << "W" << setw (6) << "." << -transactionAmount << setw(6) << "." << balance << endl; 
+        totalMoney -= transactionAmount; //updatting the balance
+        cout << "W" << "......" << -transactionAmount << "......." << totalMoney << endl; 
     }
-    
+
     //when transaction is deposit 
     else if (transactionType == 'D')
     {
-        balance += transactionAmount; //updatting the balance
-        cout << "D" << setw (6) << "." << transactionAmount << setw (6) << "." << balance << endl;
+        totalMoney += transactionAmount; //updatting the balance
+        cout << "D" << "......" << transactionAmount << "......" << totalMoney << endl;
 
     }
-    
-    
+    }
+
     //calculation for balance at the end of the day 
-    cout << "Total Balance = " << balance << endl; 
+    cout << "Total Balance = " << totalMoney << endl; 
 
     //close the files
     inFile.close();
 
-
 return 0; 
 } 
 
+/*
+5000
+TYPE......AMOUNT......BALANCE
+W......-200......4800
+W......-800......4000
+W......-40......3960
+D......90......4050
+D......100......4150
+D......1000......5150
+W......-20......5130
+Total Balance = 5130
+*/
