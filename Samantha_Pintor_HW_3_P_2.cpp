09d420902c606a77608cf760af1a97860f61b009
Samanthapintor <samanthap329@nycstudents.net>
2024-10-20 14:06:33 -0400

    Update Samantha_Pintor_HW_3_P_2.cpp

--- Samantha_Pintor_HW_3_P_2.cpp
+++ Samantha_Pintor_HW_3_P_2.cpp
@@ -1,6 +1,6 @@
 //Samantha Pintor
 //Homework 3 Part 2 
-//10-19-24
+//10-20-24
 
 #include <iostream>
 #include <fstream> 
@@ -14,48 +14,82 @@ int main ()
     ofstream outFile; //output file stream variable
 
     //creating variables 
-    int startingAmount; //variable for starting amount  
-    char transactionType; //variable for transaction type
-    float balance, numberOfTransactions, transactionAmount, totalWithdrawalValue = 0.0, totalDepositValue = 0.0; 
-   
+    int totalMoney; //variable for starting amount  
+    int numberWithdrawal = 0, numberDeposit = 0, numberTransactions = 0; //variables for number of withdrawals and deposits and transactions
+    char transactionType = 0; //variable for transaction type
+    float transactionAmount = 0, totalWithdrawalValue = 0.0, totalDepositValue = 0.0; 
+
     //open the files 
     inFile.open("inData.txt");
     outFile.open("outData.txt");
 
     //read the starting amount 
-    inFile >> startingAmount; 
-    balance = startingAmount; 
-    
-    cout << "TYPE......AMOUNT......BALANCE" << endl; //setting up the format
-    
-    setfill ("."); //set fill to periods 
-    
-    while (inFile >> transactionType >> transactionAmount)
+    inFile >> totalMoney; 
+
+    outFile  << totalMoney << endl; //cout starting amount
+
+    outFile << "TYPE......AMOUNT......BALANCE" << endl; //setting up the format
+
+    setfill ('.'); //setting fill to periods
+
+    //run the code for 7 transactions 
+    while (!inFile.eof()) //variables for inputs    
     {
-        if (transactionType == 'W') //when transaction is withdrawal
-        {
-            balance -= transactionAmount; //updatting the balance
-            totalWithdrawalValue += transactionAmount; 
-            cout << "W" << setw (6) << "." << -transactionAmount << setw(6) << "." << balance << endl; 
-        }
-        
-        else if (transactionType == 'D') //when transaction is deposit 
-        {
-            balance += transactionAmount; //updatting the balance
-            totalDepositValue += transactionAmount;
-            cout << "D" << setw (6) << "." << transactionAmount << setw (6) << "." << balance << endl;
-        }
-    }    
+        inFile >> transactionType >> transactionAmount; //reads transaction type and amount
+        numberTransactions ++; //number of transactions increases
+
     
-    //close the files
-    inFile.close(); 
-    outFile.close(); 
+        //when transaction is withdrawal
+        if (transactionType == 'W')
+        {
+            numberWithdrawal ++; 
+            totalMoney -= transactionAmount; //updatting the balance
+            totalWithdrawalValue += transactionAmount; //updatting withdrawal amount
+            outFile << "W" << "......"<< -transactionAmount << "......" << totalMoney << endl; 
+        }
+    
+        //when transaction is deposit 
+        else if (transactionType == 'D')
+        {
+            numberDeposit ++; 
+            totalMoney += transactionAmount; //updatting the balance
+            totalDepositValue += transactionAmount; //updatting withdrawal amount
+            outFile << "D" << "......" << transactionAmount << "......" << totalMoney << endl;
+    
+        }
+        }
     
     //calculation for balance at the end of the day 
-    outFile << "Ending Balance = $" << balance << endl; 
+    outFile << "Ending Balance = $" << totalMoney << endl; 
+    outFile << "Number of total transactions = " << numberTransactions << endl;
     outFile << "Total Withdrawals = $" << totalWithdrawalValue << endl; 
+    outFile << "Number of Withdrawals = " << numberWithdrawal << endl;
     outFile << "Total Deposits = $" << totalDepositValue << endl; 
+    outFile << "Number of Deposits = " << numberDeposit << endl;
 
+    //close the files
+    inFile.close();
+    outFile.close();
 
 return 0; 
 } 
+
+/*
+5000
+TYPE......AMOUNT......BALANCE
+W......-200......4800
+W......-800......4000
+W......-40......3960
+D......90......4050
+D......100......4150
+D......1000......5150
+W......-20......5130
+Ending Balance = $5130
+Number of total transactions = 7
+Total Withdrawals = $1060
+Number of Withdrawals = 4
+Total Deposits = $1190
+Number of Deposits = 3
+*/
+
+
